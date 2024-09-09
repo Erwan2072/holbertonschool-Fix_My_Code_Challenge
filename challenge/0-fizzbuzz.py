6495b005b261b5ff266b712d906bd4def6de7141
Erwan2072 <erwan.lebreton@hotmail.fr>
2024-09-09 14:43:26 +0200

    modif fizzbuzz

--- challenge/0-fizzbuzz.py
+++ challenge/0-fizzbuzz.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 import sys
 
-def fizzbuzz(n):
-    for i in range(1, n + 1):
-        if i % 3 == 0 and i % 5 == 0:
-            print("FizzBuzz", end=" ")
-        elif i % 3 == 0:
-            print("Fizz", end=" ")
-        elif i % 5 == 0:
-            print("Buzz", end=" ")
-        else:
-            print(i, end=" ")
+def fizzbuzz():
+    for i in range(1, 101):
+        if i % 3 == 0:
+            print("Fizz", end="")
+        if i % 5 == 0:
+            print("Buzz", end="")
+        if i % 3 and i % 5:
+            print(i, end="")
+        print(" ", end="")
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
